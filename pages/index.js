6a7907d6264e642db6724f6f08acc01f785a82ea
James Broad <jb@kulor.com>
2017-03-18 23:22:39 +0000

    Trying direct lib import

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,38 @@
 import React from 'react'
 import Router from 'next/router'
+import 'isomorphic-fetch'
 
 export default class extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      pagageSizes: []
+      packageSizes: []
     }
   }
 
-  getInitialProps() {
+  static async getInitialProps() {
     // import getSizes from 'package-size';
+    const pkgsApi = require('../api/pkgs')
+    const packageList = ['react','react-dom', 'preact', 'jquery']
+    const packageSizes = await pkgsApi(packageList)
+    return { packageSizes: packageSizes }
+    // const res = await fetch('http://localhost/pkgs/react,preact')
+    // const data = await res.json();
+    // return { packageSizes: data }
+
+     // return request
+     //   .get('/pkgs/react,preact')
+     //   .set('Accept', 'application/json')
+     //   .end(function(err, res){
+     //     if (err || !res.ok) {
+     //       // TODO
+     //     } else {
+     //      this.setState({
+     //        packageSizes: res.body
+     //      })
+     //      alert('yay got ' + JSON.stringify(res.body));
+     //     }
+     //   });
   }
 
   componentDidMount() {
@@ -35,7 +57,7 @@ export default class extends React.Component {
     //       )
     //     })}
 
-      
+
   }
 
 
@@ -46,7 +68,14 @@ export default class extends React.Component {
       <div className='list'>
         Hello world lol!
 
-        
+        {this.props.packageSizes.map((pkg, key)=> {
+          return (
+            <div key={key}>
+            <img src={`https://img.shields.io/badge/${pkg[0]}-${pkg[1]}-green.svg`} />
+            {pkg[0]} - {pkg[1]}
+            </div>
+          )
+        })}
       </div>
     )
   }
