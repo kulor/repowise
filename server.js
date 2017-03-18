48f9ef043577b7acb8795dad8c7ba59d8f0a8892
James Broad <jb@kulor.com>
2017-03-18 21:44:11 +0000

    Dynamic port

--- server.js
+++ server.js
@@ -21,8 +21,10 @@ app.prepare()
     return handle(req, res)
   })
 
-  server.listen(3000, (err) => {
+  const PORT = process.env.PORT || 3000;
+
+  server.listen(PORT, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log(`> Ready on http://localhost:${PORT}`)
   })
 })
