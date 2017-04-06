0f28f96ceb833373ca3a863e2b1b81a813466033
James Broad <jb@kulor.com>
2017-04-06 23:47:33 +0100

    Adding Search, proper

--- components/search.js
+++ components/search.js
@@ -1,13 +1,19 @@
 import Head from 'next/head'
 
 const Search = props => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    props.onSubmit(e.target.value);
+  };
+
   return (
-    <form className="search" action="/">
+    <form className="search" action="/" onSubmit={onSubmit}>
       <input
         value={props.value}
         type="search"
         name="query"
         placeholder="Find a package"
+        autoComplete="off"
         onChange={(e) => { return props.onChange(e.target.value) }}
       />
       <Head>
--- pages/index.js
+++ pages/index.js
@@ -7,15 +7,21 @@ import PackageList from '../components/package_list'
 import request from 'superagent'
 import 'isomorphic-fetch'
 
+import db from '../lib/db'
+
+
+
 export default class extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
       packageSizes: [],
       query: props.url.query.query || '',
+      pkgs: Object.values(props.pkgs) || [],
       loading: false
     }
   }
+
 
   static async getInitialProps () {
     const pkgs = await getHomePackages()
@@ -25,11 +31,55 @@ export default class extends React.Component {
   onChangeQuery(query) {
     this.setState({
       query: query
+    }, this.doSearch)
+  }
+
+  doSearch() {
+    var ref = db.ref().child('search');
+    var key = ref.child('request').push(
+      {
+        type:'package',
+        index:'packages2',
+        size: 50,
+        // q: this.state.query,
+        body: {
+          query: {
+            "match_phrase": {
+              // this is the field name, _all is a meta indicating any field
+              "name": this.state.query
+            }
+          }
+        }
+      }
+    ).key;
+
+    this.setState({
+      loading: true,
+      pkgs: [],
+      error: null
     })
+
+    ref.child('response/'+key).on('value', (snap) => {
+      if( !snap.exists() ) { return; } // wait until we get data
+      var dat = snap.val().hits;
+      console.log(dat)
+      if(dat.total === 0) {
+        return this.setState({
+          error: "Sorry, no results were found",
+          loading: false
+        });
+      }
+      this.setState({
+        pkgs: dat.hits.map((res) => res._source),
+        loading: false
+      })
+    });
   }
 
   getPackageList() {
+
     const packages = Object.values(this.props.pkgs)
+    return packages;
     const filteredPackages = packages.filter(pkg => {
       return pkg.name.toLowerCase().search(this.state.query.toLowerCase()) >= 0;
     })
@@ -41,17 +91,23 @@ export default class extends React.Component {
     return (
       <Page>
         <Header>
-          <Search value={this.state.query} onChange={this.onChangeQuery.bind(this)} />
+          <Search
+            value={this.state.query}
+            onChange={this.onChangeQuery.bind(this)}
+            onSubmit={this.doSearch.bind(this)} />
         </Header>
 
-        <div className="intro">
-          Welcome to the best place to discover the size of javascript libraries.
-        </div>
 
-        {this.state.loading && <div className="loading">Loading...</div>}
         {this.state.error && <div className="error">{this.state.error}</div>}
 
-        <PackageList pkgList={this.getPackageList()} />
+        {this.state.loading
+          ? <div className="loading">Loading...</div>
+          : <PackageList pkgList={this.state.pkgs} />
+        }
+
+        <div className="intro">
+          Be aware of the size of javascript libraries you depend on.
+        </div>
         <style jsx>{`
             .intro {
               font-size: 24px;
