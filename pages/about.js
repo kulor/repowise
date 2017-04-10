e2b487a83045c6ad27dc16d5c3243e8b88699139
James Broad <jb@kulor.com>
2017-04-10 22:56:44 +0100

    Better wording

--- pages/about.js
+++ pages/about.js
@@ -15,15 +15,18 @@ export default class extends React.Component {
         <Header {...this.props} />
         <div>
           <h1>About this site</h1>
-          <p>Packagemass is a service that helps savvy frontend developers be aware of package sizes.</p>
-          <p>This is an important concern as we send these packages to our users which leads to a slower experience.</p>
+          <p>Packagemass helps developers be aware of frontend package sizes.</p>
 
           <h2>Motivation</h2>
-          <p>With the ease of including frontend libraries through npm etc, it's easy to find yourself with bloated static bundles. As part of the due-dilligence phase when considering a new package (Github stars, Github issues, trial and error etc..) I wanted a means to <em>easily</em> know an approximate footprint. I didn't find anything that solved my needs, so built it myself.</p>
+          <p>With the ease of including frontend libraries through NPM (and other package managers), it's easy to find yourself bloating your bundles with third party code.</p>
+
+          <p>When considering a new package I wanted an easy means to find the size (only the parts of a repo that would be included in my project). I didn't find anything that solved my needs, so built it myself.</p>
 
           <h2>Technology</h2>
-          <p><a href="https://github.com/zeit/next.js">Next.js</a> for the web serving aspect hosted on Heroku.</p>
-          <p>The heavy lifting of downloading, gzipping and calculating package sizes are Node.js scripts (async network and disk I/O is a bonus here) that run on a queue system using <a href="https://github.com/firebase/firebase-queue">Firebase Queue</a> and is published to a Firebase Database.</p>
+          <p>The frontend and a light sprinkling of backend is written in React powered by <a href="https://github.com/zeit/next.js">Next.js</a> and Express.js hosted on Heroku.</p>
+
+          <p><a href="https://cdnjs.com/">CDNJS</a> is used as a stopgap (whilst work to stabilise a custom solution is made more reliable) to get a list of static files for a package which can be downloaded and gzipped.</p>
+          <p>The heavy lifting of downloading, gzipping and calculating package sizes are run as throttled (to be kind to CDNJS' service) Node.js scripts on a <a href="https://github.com/firebase/firebase-queue">Firebase Queue</a> and is published to a Firebase Database.</p>
           <p>Search is powered using ElasticSearch with the help of <a href="https://github.com/firebase/flashlight">Flashlight</a> to handle ingest and auto updates of the Firebase data store.</p>
 
           <h2>Contact</h2>
@@ -32,6 +35,19 @@ export default class extends React.Component {
         <style>{`
           a {
             color: #fff;
+          }
+
+          h1 {
+            font-weight: 400;
+          }
+
+          h2 {
+            font-weight: 300;
+            margin-top: 50px;
+          }
+
+          p {
+            line-height: 1.3em;
           }
         `}</style>
       </Page>
