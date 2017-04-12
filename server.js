d3af39e46b0c63c30223035d4827fa3b5811426a
James Broad <jb@kulor.com>
2017-04-12 23:17:39 +0100

    Force https on live

--- server.js
+++ server.js
@@ -7,9 +7,21 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+// Heroku header (https://devcenter.heroku.com/articles/http-routing#heroku-headers) sent when going through https enabled proxy
+const forceSsl = function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+};
+
 app.prepare()
 .then(() => {
   const server = express()
+
+  if (process.env.NODE_ENV === 'production') {
+    server.use(forceSsl);
+  }
 
   server.get('/badge/:pkg', (req, res) => {
     badge(req.params.pkg, (svg, err) => {
