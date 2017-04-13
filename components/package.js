d9221c5c079486a961725779c78eebaa29016e1a
James Broad <jb@kulor.com>
2017-04-14 00:49:54 +0100

    adding static css for cards as styles were getting silly

--- components/package.js
+++ components/package.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 const getColourForSize = (bytes) => {
   if(bytes >= 200000) return 'red';
@@ -54,6 +55,9 @@ const formatBytes = (bytes,decimals) => {
 
 export default ({ pkg }) => (
   <div className="card">
+    <Head>
+      <link href="/static/card.css" media="all" rel="stylesheet" />
+    </Head>
     <h3>
       <Link href={`/pkg?id=${pkg.name}`} as={`/package/${pkg.name}`}>
         <a>{pkg.name}</a>
@@ -78,150 +82,5 @@ export default ({ pkg }) => (
         </tr>
       </tbody>
     </table>
-
-    <style>{`
-      .card {
-        background: #fff;
-        border-radius: 3px;
-        color: #2f395a;
-        position: relative;
-        overflow: hidden;
-        padding: 20px;
-      }
-
-      @media (min-width: 400px) {
-        .card {
-          padding: 30px;
-        }
-      }
-
-      @media (min-width: 400px) {
-        .card-stat-icon {
-          position: absolute;
-          right: 30px;
-          top: 35px;
-        }
-      }
-
-      .card table {
-        border: none;
-        font-size: 13px;
-        text-align: left;
-        border-collapse: collapse;
-        margin: 10px 0;
-      }
-
-      .card table th,
-      .card table td {
-        padding: 2px 5px;
-      }
-
-      @media (min-width: 400px) {
-        .card table th,
-        .card table td {
-          padding: 2px 10px;
-        }
-      }
-
-      .card table th {
-        width: 33.333%;
-        color: #9c5479;
-        font-weight: normal;
-        font-size: 12px;
-        border-bottom: none;
-        vertical-align: bottom;
-      }
-
-      .card th:first-child,
-      .card td:first-child {
-        padding-left:0;
-      }
-
-      .card th:last-child,
-      .card td:last-child {
-        padding-right:0;
-      }
-
-      @media (min-width: 400px) {
-        .card table th {
-          font-size: 15px;
-        }
-      }
-
-      .card table td {
-        font-size: 16px;
-      }
-
-      @media (min-width: 400px) {
-        .card table td {
-          font-size: 26px;
-        }
-      }
-
-      @media (min-width: 400px) {
-        .card table {
-          font-size: 18px;
-        }
-      }
-
-      .card h3 {
-        padding: 0;
-        margin: 0;
-        font-size: 25px;
-        margin-bottom: 20px;
-        border-bottom: 1px solid rgba(0, 0, 0, 0.07);
-        padding-bottom: 20px;
-      }
-
-      .card h3 a {
-        color: #2f395a;
-        text-decoration: none;
-      }
-
-      .card h3 a:hover {
-        text-decoration: underline;
-      }
-
-      .card-version {
-        font-size: 70%;
-        opacity: 0.8;
-        font-weight: 500;
-        text-decoration:none;
-        margin-left: 0.5em;
-      }
-
-      .card .size-colour:before {
-        content: "";
-        background: green;
-        display: inline-block;
-        width: 6px;
-        height: 6px;
-        border-radius: 50%;
-        margin-right: 5px;
-        margin-bottom: 2px;
-      }
-
-      @media (min-width: 400px) {
-        .card .size-colour:before {
-          width: 12px;
-          height: 12px;
-          margin-right: 10px;
-          margin-bottom: 3px;
-        }
-      }
-
-      .card .size-colour-red:before {
-        background: #e05d44;
-      }
-      .card .size-colour-orange:before {
-        background: #fe7d37;
-      }
-      .card .size-colour-yellow:before {
-        background: #dfb317;
-      }
-      .card .size-colour-green:before {
-        background: #4c1;
-      }
-    `}</style>
   </div>
 )
