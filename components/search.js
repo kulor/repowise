85593da34582bbe9f2edf286d256b1de7e426155
James Broad <jb@kulor.com>
2017-04-24 23:17:46 +0100

    Extracting CSS and ting

--- components/search.js
+++ components/search.js
@@ -6,6 +6,10 @@ const Search = props => {
     props.onSubmit(e.target.value)
   }
 
+  const onChange = (e) => (
+    props.onChange(e.target.value)
+  )
+
   return (
     <form className="search" action="/" onSubmit={onSubmit}>
       <input
@@ -14,56 +18,10 @@ const Search = props => {
         name="query"
         placeholder="Find a package"
         autoComplete="off"
-        onChange={(e) => { return props.onChange(e.target.value) }}
+        onChange={onChange}
       />
       <Head>
-        <style>{`
-        .search {
-          position: relative;
-          padding-left: 30px;
-        }
-
-        .search:before {
-          content: "";
-          display:block;
-          width: 20px;
-          height: 20px;
-          background: url(/static/search.svg) no-repeat;
-          background-size:contain;
-          position: absolute;
-          top: 50%;
-          left: 20px;
-          transform: translateY(-50%);
-          opacity: 0.7;
-        }
-
-        .search input {
-          background: transparent;
-          padding: 20px;
-          font-size: 26px;
-          font-weight: 300;
-          color: #fff;
-          display: block;
-          width: 100%;
-          border: none;
-
-          border-radius: 0;
-          transition: border 180ms ease-in-out;
-          -webkit-appearance: none;
-          -webkit-border-radius:0px;
-        }
-
-        .search input:placeholder-shown,
-        .search input::-webkit-input-placeholder
-        {
-          color: rgba(255,255,255, 0.3)
-        }
-
-        .search input:focus {
-          outline: none;
-
-        }
-        `}</style>
+        <link href="/static/search.css" media="all" rel="stylesheet" />
       </Head>
     </form>
   );
