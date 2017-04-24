b2b13fe3237688e3ce91d4edcbc1ee403b1eaa99
James Broad <jb@kulor.com>
2017-04-24 22:36:07 +0100

    Cleanup file

--- pages/search.js
+++ pages/search.js
@@ -1,20 +1,13 @@
 import React from 'react'
 import Router from 'next/router'
-import getHomePackages from '../lib/get_home_packages'
 import Page from '../components/page'
 import Spinner from '../components/spinner'
 import Header from '../components/header'
 import Search from '../components/search'
 import PackageList from '../components/package_list'
-import request from 'superagent'
-import 'isomorphic-fetch'
-
 import algoliasearch from 'algoliasearch'
 const client = algoliasearch('LGDJZ6PHE2', '31f0cc1c00b9dc3c666a25f560448e31')
 const index = client.initIndex('packages4')
-
-
-import db from '../lib/db'
 
 export default class extends React.Component {
   constructor (props) {
@@ -80,7 +73,6 @@ export default class extends React.Component {
     return (
       <Page>
         <Header {...this.props} />
-
 
         {this.state.error && <div className="error">{this.state.error}</div>}
 
